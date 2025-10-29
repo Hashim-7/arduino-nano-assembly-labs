2ff80a6b59d83229df0b08c22993c5a18d3c95a0
Hashim-7 <155156770+Hashim-7@users.noreply.github.com>
2025-10-29 13:43:11 +0000

    Update blink.s

--- src/blink.s
+++ src/blink.s
@@ -9,6 +9,13 @@ main:	ldi r16, 0
 mainloop:
 	ldi r16, 0x01
 	out PORTB, r16
+	call halfsec
+	ldi r16, 0x00
+	out PORTB, r16
+	call halfsec
+rjmp mainloop
+
+halfsec:
 	ldi r17, 25
 	outer:
 		ldi r18, 248
@@ -25,22 +32,4 @@ mainloop:
 		dec r17
 		cpi r17, 0
 		brne outer
-	ldi r16, 0x00
-	out PORTB, r16
-	ldi r17, 25
-	outer2:
-		ldi r18, 248
-		middle2:
-			ldi r19, 255
-			inner2:
-				nop
-				dec r19
-				cpi r19, 0
-				brne inner2
-			dec r18
-			cpi r18, 0
-			brne middle2
-		dec r17
-		cpi r17, 0
-		brne outer2
-rjmp mainloop
+	ret
